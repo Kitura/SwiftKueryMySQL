a9e4a0d061e098c0f00b6590f7b4830c08167fb6
Navneet Gupta <navneet@us.ibm.com>
2017-02-24 16:18:32 -0600

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -20,6 +20,6 @@ let package = Package(
     name: "SwiftKueryMySQL",
     dependencies: [
         .Package(url: "https://github.ibm.com/IBM-Swift/CMySQL.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", majorVersion: 0, minor: 6),
     ]
 )
