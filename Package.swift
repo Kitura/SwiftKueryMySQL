cd6951c0b033df1d80782b4d4540060f20cc0980
Ira Rosen <IRAR@il.ibm.com>
2017-03-30 13:34:01 +0300

    IBM-Swift/Kitura#1047 Swift 3.1 changes

--- Package.swift
+++ Package.swift
@@ -20,6 +20,6 @@ let package = Package(
     name: "SwiftKueryMySQL",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/CMySQL.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", majorVersion: 0, minor: 9),
+        .Package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", majorVersion: 0, minor: 11),
     ]
 )
