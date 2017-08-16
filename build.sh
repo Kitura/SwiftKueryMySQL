5a9d04e844e9a6d2c7059986d6f600b9670dcfd1
Navneet Gupta <navneet@us.ibm.com>
2017-08-16 10:08:02 -0500

    Add swift 4 builds

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o verbose
 
 if [ -n "${DOCKER_IMAGE}" ]; then
     docker pull ${DOCKER_IMAGE}
-    docker run --env SWIFT_VERSION -v ${TRAVIS_BUILD_DIR}:${TRAVIS_BUILD_DIR} ${DOCKER_IMAGE} /bin/bash -c "apt-get update && apt-get install -y git sudo lsb-release wget libxml2 && cd $TRAVIS_BUILD_DIR && ./build.sh"
+    docker run --env SWIFT_SNAPSHOT -v ${TRAVIS_BUILD_DIR}:${TRAVIS_BUILD_DIR} ${DOCKER_IMAGE} /bin/bash -c "apt-get update && apt-get install -y git sudo lsb-release wget libxml2 && cd $TRAVIS_BUILD_DIR && ./build.sh"
 else
     if [[ $TRAVIS_OS_NAME == "osx" ]]; then
         mysql --version || { brew update && brew install mysql && mysql.server start && mysql --version; }
@@ -18,7 +18,6 @@ else
     mysql -uroot -e "CREATE DATABASE IF NOT EXISTS test;"
     mysql -uroot -e "GRANT ALL ON test.* TO 'swift'@'localhost';"
 
-    test -n "${SWIFT_VERSION}" && echo "${SWIFT_VERSION}" > .swift-version || echo "SWIFT_VERSION not set, using $(cat .swift-version)"
     git clone https://github.com/IBM-Swift/Package-Builder.git
     ./Package-Builder/build-package.sh -projectDir $(pwd)
 fi
