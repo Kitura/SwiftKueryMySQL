3fcfa7bb098be58da30d6e89d81923bfc7c59b3a
kilnerm@uk.ibm.com <kilnerm@uk.ibm.com>
2018-09-27 14:28:01 +0100

    Add mysql version support to build script

--- build.sh
+++ build.sh
@@ -10,18 +10,28 @@ if [ -n "${DOCKER_IMAGE}" ]; then
 else
 
     if [[ $TRAVIS_OS_NAME == "osx" ]]; then
-        mysql --version || { brew update && brew install mysql@5.7 && brew link mysql@5.7 --force && mysql.server start && mysql --version; }
+        if [[ $MYSQL_VER == 5 ]]; then
+            mysql --version || { brew update && brew install mysql@5.7 && brew link mysql@5.7 --force && mysql.server start && mysql --version; }
+        else
+            mysql --version || { brew update && brew install mysql && mysql.server start && mysql --version; }
+        fi
     else
         export DEBIAN_FRONTEND="noninteractive"
-        echo mysql-community-server mysql-community-server/root-pass password | debconf-set-selections
-        sudo apt-get update -y
-        sudo -E apt-get install -y pkg-config
-        sudo -E apt-get install -q -y mysql-server
-        sudo -E apt-get install -y libmysqlclient-dev
-        service mysql start
-        mysql --version
-        echo "** Finding mysqlclient.pc **"
-        find / -name mysqlclient.pc
+        if [[ $MYSQL_VER == 5 ]]; then
+            mysql --version || { apt-get update && apt-get install -y mysql-server libmysqlclient-dev && service mysql start && mysql --version; }
+        else
+            cd /tmp
+            wget https://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb
+            cd -
+            echo mysql-apt-config mysql-apt-config/select-server select mysql-8.0 | debconf-set-selections
+            dpkg -i /tmp/mysql-apt-config_0.8.10-1_all.deb
+            echo mysql-community-server mysql-community-server/root-pass password | debconf-set-selections
+            apt-get update -y
+            apt-get install -q -y mysql-server
+            apt-get install -y libmysqlclient-dev
+            service mysql start
+            mysql --version
+        fi
     fi
 
     mysql_upgrade -uroot || echo "No need to upgrade"
