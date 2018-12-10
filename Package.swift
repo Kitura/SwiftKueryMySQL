96cfc4ecd3221b5f1e0a752ba2678d9fdce1f6e4
David Jones <djones6@uk.ibm.com>
2018-12-10 13:03:38 +0000

    Add providers

--- Package.swift
+++ Package.swift
@@ -36,7 +36,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .systemLibrary(
             name: "CMySQL",
-            pkgConfig: "mysqlclient"
+            pkgConfig: "mysqlclient",
+            providers: [
+                .brew(["mysql"]),
+                .apt(["libmysqlclient-dev"])
+            ]
         ),
         .target(
             name: "SwiftKueryMySQL",
